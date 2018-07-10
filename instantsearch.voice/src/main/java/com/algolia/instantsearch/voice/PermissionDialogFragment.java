8a0a9868be690948a580e7bd76c8157a4b0f4387
Paul-Louis NECH <pln@algolia.com>
2018-07-10 13:27:28 +0200

    feat(PermissionDialogFragment): Customize copy

--- instantsearch.voice/src/main/java/com/algolia/instantsearch/voice/PermissionDialogFragment.java
+++ instantsearch.voice/src/main/java/com/algolia/instantsearch/voice/PermissionDialogFragment.java
@@ -13,6 +13,15 @@ import static android.Manifest.permission.RECORD_AUDIO;
 
 public class PermissionDialogFragment extends DialogFragment {
     public static final int ID_REQ_VOICE_PERM = 1;
+    public static final String DEFAULT_TITLE = "You can use voice search to find results";
+    public static final String DEFAULT_MESSAGE = "Can we access your device's microphone to enable voice search?";
+    public static final String DEFAULT_POSITIVE_BUTTON = "Allow microphone access";
+    public static final String DEFAULT_NEGATIVE_BUTTON = "No";
+
+    private String title = DEFAULT_TITLE;
+    private String message = DEFAULT_MESSAGE;
+    private String positiveButton = DEFAULT_POSITIVE_BUTTON;
+    private String negativeButton = DEFAULT_NEGATIVE_BUTTON;
 
     @NonNull
     @Override
@@ -22,16 +31,16 @@ public class PermissionDialogFragment extends DialogFragment {
             throw new IllegalStateException("PermissionDialogFragment must be used within an activity.");
         }
         return new AlertDialog.Builder(activity)
-                .setTitle("You can use voice search to find products")
-                .setMessage("Can we access your device's microphone to enable voice search?")
-                .setPositiveButton("Allow microphone access", new DialogInterface.OnClickListener() {
+                .setTitle(title)
+                .setMessage(message)
+                .setPositiveButton(positiveButton, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         ActivityCompat.requestPermissions(activity, new String[]{RECORD_AUDIO}, ID_REQ_VOICE_PERM);
                         dismiss();
                     }
                 })
-                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                .setNegativeButton(negativeButton, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         dialog.cancel();
@@ -39,4 +48,22 @@ public class PermissionDialogFragment extends DialogFragment {
                 })
                 .create();
     }
+
+    //region setters
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setPositiveButton(String positiveButton) {
+        this.positiveButton = positiveButton;
+    }
+
+    public void setNegativeButton(String negativeButton) {
+        this.negativeButton = negativeButton;
+    }
+    //endregion
 }
