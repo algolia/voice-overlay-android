7a69c4b8debf09585510e06d5ac6a9ef629d429a
Paul-Louis NECH <pln@algolia.com>
2018-08-17 17:11:56 +0200

    App: minimal demo alongside library

---
+++ app/src/main/java/com/algolia/instantsearch/voice/demo/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.algolia.instantsearch.voice.demo
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.algolia.instantsearch.voice.Voice
+import com.algolia.instantsearch.voice.ui.PermissionDialogFragment
+import com.algolia.instantsearch.voice.ui.VoiceDialogFragment
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), VoiceDialogFragment.VoiceResultsListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            if (!Voice.hasRecordPermission(this)) {
+                PermissionDialogFragment().show(supportFragmentManager, "perm")
+            } else {
+                val dialogFragment = VoiceDialogFragment()
+                dialogFragment.voiceResultsListener = this
+                dialogFragment.show(supportFragmentManager, "voice")
+            }
+        }
+    }
+
+    override fun onVoiceResults(matches: List<String>) {
+        textView.text = matches[0]
+    }
+}
