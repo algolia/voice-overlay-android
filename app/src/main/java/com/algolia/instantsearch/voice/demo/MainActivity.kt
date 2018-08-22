ef2f540e2183afd5f8546e214a10982e475898f4
Quentin LITZLER <qlitzler@gmail.com>
2018-08-22 11:27:10 +0200

    refactor: Permission dialog now uses arguments.

--- app/src/main/java/com/algolia/instantsearch/voice/demo/MainActivity.kt
+++ app/src/main/java/com/algolia/instantsearch/voice/demo/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.algolia.instantsearch.voice.demo
 
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import com.algolia.instantsearch.voice.Voice
 import com.algolia.instantsearch.voice.ui.PermissionDialogFragment
@@ -16,7 +17,10 @@ class MainActivity : AppCompatActivity(), VoiceInput.VoiceResultsListener {
 
         button.setOnClickListener {
             if (!Voice.hasRecordPermission(this)) {
-                PermissionDialogFragment().show(supportFragmentManager, "perm")
+                PermissionDialogFragment().let {
+                    it.arguments = PermissionDialogFragment.buildArguments(title = "Voice Search.")
+                    it.show(supportFragmentManager, "perm")
+                }
             } else {
                 val voiceFragment = VoiceDialogFragment() //FIXME: Handle orientation changes, storing state properly
                 voiceFragment.setSuggestions("Something", "Something else")
